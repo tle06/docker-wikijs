f5acfedc4821ce16af9739d126f225057983f43e
Thomas ltp <tle@tlnk.fr>
2018-09-02 18:20:58 +0200

    yml space

--- entrypoint/entrypoint.d/0.sh
+++ entrypoint/entrypoint.d/0.sh
@@ -108,168 +108,168 @@ else
     #Write configuration
     echo "Writing configuration into $file"
 
-    echo "
-    #######################################################################
-    # Wiki.js - CONFIGURATION                                             #
-    #######################################################################
-    # Full explanation + examples in the documentation:
-    # https://docs.requarks.io/wiki/install
-    # You can use an ENV variable by using \$(ENV_VAR_NAME) as the value
+echo "
+#######################################################################
+# Wiki.js - CONFIGURATION                                             #
+#######################################################################
+# Full explanation + examples in the documentation:
+# https://docs.requarks.io/wiki/install
+# You can use an ENV variable by using \$(ENV_VAR_NAME) as the value
 
-    # ---------------------------------------------------------------------
-    # Title of this site
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Title of this site
+# ---------------------------------------------------------------------
 
-    title: ${VAR_TITLE}
+title: ${VAR_TITLE}
 
-    # ---------------------------------------------------------------------
-    # Full public path to the site, without the trailing slash
-    # ---------------------------------------------------------------------
-    # INCLUDE CLIENT PORT IF NOT 80/443!
+# ---------------------------------------------------------------------
+# Full public path to the site, without the trailing slash
+# ---------------------------------------------------------------------
+# INCLUDE CLIENT PORT IF NOT 80/443!
 
-    host: ${VAR_HOST}
+host: ${VAR_HOST}
 
-    # ---------------------------------------------------------------------
-    # Port the main server should listen to (80 by default)
-    # ---------------------------------------------------------------------
-    # To use process.env.PORT, comment the line below:
+# ---------------------------------------------------------------------
+# Port the main server should listen to (80 by default)
+# ---------------------------------------------------------------------
+# To use process.env.PORT, comment the line below:
 
-    port: ${VAR_PORT}
+port: ${VAR_PORT}
 
-    # ---------------------------------------------------------------------
-    # Data Directories
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Data Directories
+# ---------------------------------------------------------------------
 
-    paths:
-    repo: ${VAR_REPO_PATH}
-    data: ${VAR_DATA_PATH}
+paths:
+  repo: ${VAR_REPO_PATH}
+  data: ${VAR_DATA_PATH}
 
-    # ---------------------------------------------------------------------
-    # Upload Limits
-    # ---------------------------------------------------------------------
-    # In megabytes (MB}
+# ---------------------------------------------------------------------
+# Upload Limits
+# ---------------------------------------------------------------------
+# In megabytes (MB}
 
-    uploads:
-    maxImageFileSize: ${VAR_MAX_IMAGE_FILE_SIZE}
-    maxOtherFileSize: ${VAR_MAX_OTHER_FILE_SIZE}
+uploads:
+  maxImageFileSize: ${VAR_MAX_IMAGE_FILE_SIZE}
+  maxOtherFileSize: ${VAR_MAX_OTHER_FILE_SIZE}
 
-    # ---------------------------------------------------------------------
-    # Site Language
-    # ---------------------------------------------------------------------
-    # Possible values: en, de, es, fa, fr, ja, ko, nl, pt, ru, sr, sv, tr or zh
+# ---------------------------------------------------------------------
+# Site Language
+# ---------------------------------------------------------------------
+# Possible values: en, de, es, fa, fr, ja, ko, nl, pt, ru, sr, sv, tr or zh
 
-    lang: ${VAR_LANG}
+lang: ${VAR_LANG}
 
-    # Enable for right to left languages (e.g. arabic):
-    langRtl: ${VAR_LANG_RTL}
+# Enable for right to left languages (e.g. arabic):
+langRtl: ${VAR_LANG_RTL}
 
-    # ---------------------------------------------------------------------
-    # Site Authentication
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Site Authentication
+# ---------------------------------------------------------------------
 
-    public: ${VAR_PUBLIC_ACCESS}
+public: ${VAR_PUBLIC_ACCESS}
 
-    auth:
-        defaultReadAccess: ${VAR_AUTH_DEFAULT_READ_ACCESS}
-    local:
-        enabled: ${VAR_AUTH_LOCAL}
-    google:
-        enabled: ${VAR_AUTH_GOOGLE}
-        clientId: ${VAR_AUTH_GG_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_GG_CLIENT_SECRET}
-    microsoft:
-        enabled: ${VAR_AUTH_MICROSOFT}
-        clientId: ${VAR_AUTH_MS_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_MS_CLIENT_SECRET}
-    facebook:
-        enabled: ${VAR_AUTH_FACEBOOK}
-        clientId: ${VAR_AUTH_FB_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_FB_CLIENT_SECRET}
-    github:
-        enabled: ${VAR_AUTH_GITHUB}
-        clientId: ${VAR_AUTH_GH_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_GH_CLIENT_SECRET}
-    slack:
-        enabled: ${VAR_AUTH_SLACK}
-        clientId: ${VAR_AUTH_SK_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_CLIENT_SECRET}
-    ldap:
-        enabled: ${VAR_AUTH_LDAP}
-        url: ${VAR_AUTH_LDAP_URL}
-        bindDn: ${VAR_AUTH_LDAP_BIND_DN}
-        bindCredentials: ${VAR_AUTH_LDAP_BIND_CREDENTIALS}
-        searchBase: ${VAR_AUTH_LDAP_SEARCH_BASE}
-        searchFilter: ${VAR_AUTH_LDAP_SEARCH_FILTER}
-        tlsEnabled: ${VAR_AUTH_LDAP_TLS_ENABLE}
-        tlsCertPath: ${VAR_AUTH_LDAP_TLS_CERT_PATH}
-    azure:
-        enabled: ${VAR_AUTH_AZURE}
-        clientId: ${VAR_AUTH_AZ_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_AZ_CLIENT_SECRET}
-        resource: ${VAR_AUTH_AZ_RESOURCE}
-        tenant: ${VAR_AUTH_AZ_TENANT}
-    oauth2:
-        enabled: ${VAR_AUTH_OAUTH2}
-        clientId: ${VAR_AUTH_OH_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_OH_CLIENT_SECRET}
-        authorizationURL: ${VAR_AUTH_OH_AUTHORIZATION_URL}
-        tokenURL: ${VAR_AUTH_OH_TOKEN_URL}
-    oidc:
-        enabled: ${VAR_AUTH_OIDC}
-        clientId: ${VAR_AUTH_OC_CLIENT_ID}
-        clientSecret: ${VAR_AUTH_OC_CLIENT_SECRET}
-        issuer: ${VAR_AUTH_OC_ISSUER}
-        userInfoUrl: ${VAR_AUTH_OC_USER_INFO_URL}
-        authorizationURL: ${VAR_AUTH_OC_AUTHORIZATION_URL}
-        tokenURL: ${VAR_AUTH_OC_TOKEN_URL}
-        emailClaim: ${VAR_AUTH_OC_EMAIL_CLAIM}
-        usernameClaim: ${VAR_AUTH_OC_USERNAME_CLAIM}
-    # ---------------------------------------------------------------------
-    # Secret key to use when encrypting sessions
-    # ---------------------------------------------------------------------
-    # Use a long and unique random string (256-bit keys are perfect!)
+auth:
+  defaultReadAccess: ${VAR_AUTH_DEFAULT_READ_ACCESS}
+  local:
+    enabled: ${VAR_AUTH_LOCAL}
+  google:
+    enabled: ${VAR_AUTH_GOOGLE}
+    clientId: ${VAR_AUTH_GG_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_GG_CLIENT_SECRET}
+  microsoft:
+    enabled: ${VAR_AUTH_MICROSOFT}
+    clientId: ${VAR_AUTH_MS_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_MS_CLIENT_SECRET}
+  facebook:
+    enabled: ${VAR_AUTH_FACEBOOK}
+    clientId: ${VAR_AUTH_FB_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_FB_CLIENT_SECRET}
+  github:
+    enabled: ${VAR_AUTH_GITHUB}
+    clientId: ${VAR_AUTH_GH_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_GH_CLIENT_SECRET}
+  slack:
+    enabled: ${VAR_AUTH_SLACK}
+    clientId: ${VAR_AUTH_SK_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_CLIENT_SECRET}
+  ldap:
+    enabled: ${VAR_AUTH_LDAP}
+    url: ${VAR_AUTH_LDAP_URL}
+    bindDn: ${VAR_AUTH_LDAP_BIND_DN}
+    bindCredentials: ${VAR_AUTH_LDAP_BIND_CREDENTIALS}
+    searchBase: ${VAR_AUTH_LDAP_SEARCH_BASE}
+    searchFilter: ${VAR_AUTH_LDAP_SEARCH_FILTER}
+    tlsEnabled: ${VAR_AUTH_LDAP_TLS_ENABLE}
+    tlsCertPath: ${VAR_AUTH_LDAP_TLS_CERT_PATH}
+  azure:
+    enabled: ${VAR_AUTH_AZURE}
+    clientId: ${VAR_AUTH_AZ_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_AZ_CLIENT_SECRET}
+    resource: ${VAR_AUTH_AZ_RESOURCE}
+    tenant: ${VAR_AUTH_AZ_TENANT}
+  oauth2:
+    enabled: ${VAR_AUTH_OAUTH2}
+    clientId: ${VAR_AUTH_OH_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_OH_CLIENT_SECRET}
+    authorizationURL: ${VAR_AUTH_OH_AUTHORIZATION_URL}
+    tokenURL: ${VAR_AUTH_OH_TOKEN_URL}
+  oidc:
+    enabled: ${VAR_AUTH_OIDC}
+    clientId: ${VAR_AUTH_OC_CLIENT_ID}
+    clientSecret: ${VAR_AUTH_OC_CLIENT_SECRET}
+    issuer: ${VAR_AUTH_OC_ISSUER}
+    userInfoUrl: ${VAR_AUTH_OC_USER_INFO_URL}
+    authorizationURL: ${VAR_AUTH_OC_AUTHORIZATION_URL}
+    tokenURL: ${VAR_AUTH_OC_TOKEN_URL}
+    emailClaim: ${VAR_AUTH_OC_EMAIL_CLAIM}
+    usernameClaim: ${VAR_AUTH_OC_USERNAME_CLAIM}
+# ---------------------------------------------------------------------
+# Secret key to use when encrypting sessions
+# ---------------------------------------------------------------------
+# Use a long and unique random string (256-bit keys are perfect!)
 
-    sessionSecret: ${VAR_SESSION_SECRET}
+sessionSecret: ${VAR_SESSION_SECRET}
 
-    # ---------------------------------------------------------------------
-    # Database Connection String
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Database Connection String
+# ---------------------------------------------------------------------
 
-    db: ${VAR_DB_CONNECTION_STRING}
+db: ${VAR_DB_CONNECTION_STRING}
 
 
-    # ---------------------------------------------------------------------
-    # Git Connection Info
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Git Connection Info
+# ---------------------------------------------------------------------
 
-    git: ${VAR_GIT_ENABLE}" > $file
+git: ${VAR_GIT_ENABLE}" > $file
 
     if [ "${VAR_GIT_ENABLE}" = true ]; then
-    echo "GIT_ENABLE = true"
-    echo "Writing GIT config into /var/wiki/config/yml"
+        echo "GIT_ENABLE = true"
+echo "Writing GIT config into /var/wiki/config/yml"
 
-    echo "
-    url: ${VAR_GH_URL:-https://github.com/Organization/Repo}
-    branch: ${VAR_GH_BRANCH:-master}
-    auth:
+echo "
+  url: ${VAR_GH_URL:-https://github.com/Organization/Repo}
+  branch: ${VAR_GH_BRANCH:-master}
+  auth:
 
-        # Type: basic or ssh
-        type: ${VAR_GH_AUTH_TYPE:-ssh}
+    # Type: basic or ssh
+    type: ${VAR_GH_AUTH_TYPE:-ssh}
 
-        # Only for Basic authentication:
-        username: ${VAR_GH_USERNAME:-marty}
-        password: ${VAR_GH_PASSWORD:-MartyMcFly88}
+    # Only for Basic authentication:
+    username: ${VAR_GH_USERNAME:-marty}
+    password: ${VAR_GH_PASSWORD:-MartyMcFly88}
 
-        # Only for SSH authentication:
-        privateKey: ${VAR_GH_PRIVATE_KEY_PATH:-/etc/wiki/keys/git.pem}
+    # Only for SSH authentication:
+    privateKey: ${VAR_GH_PRIVATE_KEY_PATH:-/etc/wiki/keys/git.pem}
 
-        sslVerify: ${VAR_GH_SSL_VERIFY}
+    sslVerify: ${VAR_GH_SSL_VERIFY}
 
-    # Default email to use as commit author
-    serverEmail: ${VAR_GH_SERVER_EMAIL:-marty@example.com}
+# Default email to use as commit author
+  serverEmail: ${VAR_GH_SERVER_EMAIL:-marty@example.com}
 
-    # Whether to use user email as author in commits
-    showUserEmail: ${VAR_GH_SHOVAR_USER_EMAIL}" >> $file
+# Whether to use user email as author in commits
+  showUserEmail: ${VAR_GH_SHOVAR_USER_EMAIL}" >> $file
 
 
     else
@@ -281,44 +281,44 @@ else
 
     echo "Writing remaining config into /var/wiki/config.yml"
 
-    echo "
-    # ---------------------------------------------------------------------
-    # Features
-    # ---------------------------------------------------------------------
-    # You can enable / disable specific features below
+echo "
+# ---------------------------------------------------------------------
+# Features
+# ---------------------------------------------------------------------
+# You can enable / disable specific features below
 
-    features:
-    linebreaks: ${VAR_FT_LINE_BREAKS}
-    mathjax: ${VAR_FT_MATHJAX}
+features:
+  linebreaks: ${VAR_FT_LINE_BREAKS}
+  mathjax: ${VAR_FT_MATHJAX}
 
-    # ---------------------------------------------------------------------
-    # External Logging
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# External Logging
+# ---------------------------------------------------------------------
 
-    externalLogging:
-    bugsnag: ${VAR_EL_BUGSNAG_ENABLE}
-    loggly: ${VAR_EL_LOGGLY_ENABLE}
-    papertrail: ${VAR_EL_PAPERTRAIL_ENABLE}
-    rollbar: ${VAR_EL_ROLLBAR_ENABLE}
-    sentry: ${VAR_EL_SENTRY_ENABLE}
+externalLogging:
+  bugsnag: ${VAR_EL_BUGSNAG_ENABLE}
+  loggly: ${VAR_EL_LOGGLY_ENABLE}
+  papertrail: ${VAR_EL_PAPERTRAIL_ENABLE}
+  rollbar: ${VAR_EL_ROLLBAR_ENABLE}
+  sentry: ${VAR_EL_SENTRY_ENABLE}
 
-    # ---------------------------------------------------------------------
-    # Color Theme
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Color Theme
+# ---------------------------------------------------------------------
 
-    theme:
-    primary: ${VAR_THEME_PRIMARY}
-    alt: ${VAR_THEME_ALT}
-    viewSource: ${VAR_THEME_VIEWSOURCE}
-    footer: ${VAR_THEME_FOOTER}
-    code:
-        dark: ${VAR_THEME_CODE_ENABLE}
-        colorize: ${VAR_THEME_COLORIZE_ENABLE}
+theme:
+  primary: ${VAR_THEME_PRIMARY}
+  alt: ${VAR_THEME_ALT}
+  viewSource: ${VAR_THEME_VIEWSOURCE}
+  footer: ${VAR_THEME_FOOTER}
+  code:
+    dark: ${VAR_THEME_CODE_ENABLE}
+    colorize: ${VAR_THEME_COLORIZE_ENABLE}
 
-    #" >> $file
+#" >> $file
 
 
-    echo "Wiki.js config completed"
+echo "Wiki.js config completed"
 fi
 
 #debug
